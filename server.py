7b1bc5824e5a12fa2f10929078dd050b4aea8725
Xavier Tondelier <xtondelier@gmail.com>
2025-10-10 13:15:56 +0100

    Add basic validation for IP and PORT

--- server.py
+++ server.py
@@ -1,7 +1,30 @@
-import socket
+import socket, sys, re
 
-ip = "127.0.0.1" # localhost
-port = 12345
+#Usage - python(3) server.py ip port
+
+if len(sys.argv) != 3:
+    print("Must provide IP address and port number.")
+    sys.exit()
+else:
+    try:
+        valid = re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", sys.argv[1])
+        if valid == None:
+            raise Exception("Invalid IP address format.")
+        else:
+            ip = sys.argv[1] # localhost
+    except Exception as error:
+        print("Problem with IP:")
+        print(error)
+        sys.exit()
+
+    try:
+        if int(sys.argv[2]) > 65535 or int(sys.argv[2]) < 0:
+            raise Exception("Port must be between 0 and 65536.")
+        port = int(sys.argv[2])
+    except Exception as error:
+        print("Problem with port:")
+        print(error)
+        sys.exit()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
